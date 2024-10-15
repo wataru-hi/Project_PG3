740ab0d08c8a9a9f92eb9dd79143047102b67ca9
wataru-hi <146914543+wataru-hi@users.noreply.github.com>
2024-10-15 12:52:57 +0900

    01-02確認課題

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
+int calculate_salary(int month) {
+  if (month <= 0) { 
+    return 0; // –³Œø‚ÈŒ‚Í0‚ğ•Ô‚·
+  }
+
+  int salary = 100; // ‰Œ‚Ì‹‹—¿‚ğİ’è
+
+  for (int i = 2; i <= month; i++) {
+    salary = salary * 2 - 50; 
+  }
+
+  return salary;
+}
+
 int main() 
 {
-	printf("test‚ ");
+    int month = 1;
+    int normalSalary = 1072;
+    int salaly = normalSalary, recursiveSalaly = 100;
+    while (salaly > recursiveSalaly)
+    {
+        salaly = normalSalary * month;
+        recursiveSalaly = calculate_salary(month);
+        month ++;
+    }
+
+    printf("%dŒ‚©‚çÄ‹A“I‚È’À‹à‘ÌŒn‚Ì‚Ù‚¤‚ª–×‚©‚é", month);
 }
